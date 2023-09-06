5c6267c694b2daafffb033de311f3f0135abff00
WGiligili <81459687+WGiligili@users.noreply.github.com>
2023-09-06 11:13:32 +0800

    verson2

--- painter/js/scripts.js
+++ painter/js/scripts.js
@@ -1,95 +1,109 @@
-const canvas = document.getElementById('drawingCanvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById('drawingCanvas');//獲取元素
+const context = canvas.getContext('2d');//獲取上下文
 const clearButton = document.getElementById('clearButton');
 const Pressed =document.getElementById('Pressed');
 const lineWidthRange = document.getElementById('lineWidthRange');
 
+//畫布放置位子移動距離
 let cutwidth =  window.innerWidth * 0.1 ;
 let cutheight = window.innerWidth * 0.1 ;
-
-canvas.width = window.innerWidth - cutwidth;
+//畫布大小
+canvas.width =  window.innerWidth - cutwidth;
 canvas.height = window.innerHeight - cutheight;
+
+
 let drawing = false;
 let lastX = 0;
 let lastY = 0;
-let lineWidth = lineWidthRange.value;
-let pressValue = Pressed.value;
 
-// 初始化画布大小
-//resizeCanvas();
-// 当窗口大小更改时，调整画布大小
+let pressValue = Pressed.value;
+let lineWidth = lineWidthRange.value + pressValue;
+
+
+//顏色與線條粗細
+context.strokeStyle = 'black';
+context.lineWidth = lineWidth + Pressed.value;
+
+// 初始化画布大小, 当窗口大小更改时，调整画布大小
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas() {
     // 设置画布的宽度和高度与窗口大小一致
-    canvas.width = window.innerWidth -cutwidth; 
-    canvas.height = window.innerHeight -cutheight;
+    canvas.width = window.innerWidth - cutwidth; 
+    canvas.height = window.innerHeight - cutheight ;
 }
-
-// 开始绘画
-canvas.addEventListener('touchstart', (e) => {
-    drawing = true;
-    [lastX, lastY] = [e.touches[0].clientX+cutwidth, e.touches[0].clientY+cutheight];
-
-    context.beginPath();
-    context.moveTo(lastX, lastY);
-    draw( lastX, lastY);
-   // draw(e.touches[0].clientX, e.touches[0].clientY);
-});
-
-function draw(x, y) {
-    
-    context.lineWidth = lineWidth; // 设置线条宽度
-    context.lineCap = 'round'; // 设置线条末端为圆形
-    context.strokeStyle = 'blue'; // 设置线条颜色
-
-    context.lineTo(x, y);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(x, y);
-}
-
-
-// 结束绘画
-canvas.addEventListener('touchend', () => {
-    drawing = false;
-    context.beginPath();
-});
-
 // 调整线条粗细
 lineWidthRange.addEventListener('input', () => {
     lineWidth = lineWidthRange.value;
 });
 
 
+
+
+///繪畫事件/// 滑鼠與觸控 控制元件不同需要分開寫
+// 滑鼠
+canvas.addEventListener('mousedown', (e) => {
+    drawing = true;
+    [lastX, lastY] = [e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top];
+});
+
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+});
+canvas.addEventListener('mousemove', draw);
+//觸控
+canvas.addEventListener('touchstart', touchStart);
+canvas.addEventListener('touchmove', touchMove);
+canvas.addEventListener('touchend', () => {
+    drawing = false;
+});
+
+//清除按鈕
 clearButton.addEventListener('click', () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
 });
-// 绘画中
-canvas.addEventListener('touchmove', (e) => {
+//滑鼠繪製
+function draw(e) {
     if (!drawing) return;
-    const pressure = e.touches[0].force || 0.5; // 获取压力值，如果不可用则使用默认值
-    Pressed.textContent = pressure;
-    
-    lineWidth = pressure * 20; // 基于压力值调整线条粗细
-    lineWidthRange.value = lineWidth;
+
     context.lineWidth = lineWidth;
     context.lineCap = 'round';
     context.strokeStyle = 'black';
-    context.lineTo(e.touches[0].clientX, e.touches[0].clientY);
+
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+    context.stroke();//線條
+    //更新移動點
+    [lastX, lastY] = [e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top];
+}
+function touchStart(e) {
+    drawing = true;
+    const touch = e.touches[0];
+    [lastX, lastY] = [touch.clientX - canvas.getBoundingClientRect().left, touch.clientY - canvas.getBoundingClientRect().top];
+}
+
+function touchMove(e) {
+    if (!drawing) return;
+
+    const touch = e.touches[0];
+    context.lineWidth = lineWidth;
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(touch.clientX - canvas.getBoundingClientRect().left, touch.clientY - canvas.getBoundingClientRect().top);
     context.stroke();
 
-    
-    const textWidth = context.measureText(text).width;
-    const x = canvas.width;
-    const y = canvas.height;
-    context.fillText(text, x, y);
-    
-    [lastX, lastY] = [e.touches[0].clientX +cutwidth, e.touches[0].clientY +cutheight];
-    draw( lastX, lastY);
-   // draw(e.touches[0].clientX, e.touches[0].clientY);   
-   
+    [lastX, lastY] = [touch.clientX - canvas.getBoundingClientRect().left, touch.clientY - canvas.getBoundingClientRect().top];
+}
+
+// 添加触控笔感压值事件处理
+canvas.addEventListener('pointermove', (e) => {
+    if (e.pointerType === 'pen') {
+        Pressed.value = e.pressure;
+        lineWidth = e.pressure * 20; // 调整线条粗细基于压感值
+    }
 });
- // 显示压力值文本
- context.font = '16px Arial';
- context.fillStyle = 'red';
- const text = `Pressure: ${pressure.toFixed(2)}`;
+
+console.log('pressValue:'+pressValue ,'lineWidth:'+ lineWidth );
